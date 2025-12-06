a34c8f20c877a45d03b9f0b83b973614e123a92b
Peter H. Boling <peter.boling@gmail.com>
2025-12-05 19:45:39 -0700

    🎉 Tada!

---
+++ spec/dotenv/merge_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Dotenv::Merge do
+  it "has a version number" do
+    expect(Dotenv::Merge::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
